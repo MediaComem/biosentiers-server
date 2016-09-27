aaf34c65b3ede50fa36f5f32256eb664b0a4d4d2
Simon Oulevay <simon.oulevay@gmail.com>
2016-09-27 17:30:46 +0200

    Apidoc: user request examples.

--- server/api/users/users.routes.js
+++ server/api/users/users.routes.js
@@ -8,13 +8,37 @@ var router = express.Router();
  * @api {POST} /users Create a user
  * @apiGroup Users
  * @apiName CreateUser
- * @apiDescription Register a new BioSentiers user with an e-mail and password.
+ * @apiDescription Register a new BioSentiers user identified by an e-mail address.
+ *
+ * The user will receive an e-mail at the specified address, containing a link.
+ * This link will include a Bearer authentication token which can be used to
+ * [set the user's password](#api-Users-UpdateUser) and complete the registration.
  *
  * @apiUse Authorization
  * @apiUse Validation
  * @apiUse UserResponse
  *
  * @apiParam (JSON parameters) {String} email A unique e-mail identifying the user.
+ *
+ * @apiExample Example
+ * POST /api/users HTTP/1.1
+ * Authorization: Bearer eyJhbGciOiJI.eyJzdWIiOiIx.Rq8IxqeX7eA6
+ * Content-Type: application/json
+ *
+ * {
+ *   "email": "jdoe@example.com"
+ * }
+ *
+ * @apiSuccessExample Success 201
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ *
+ * {
+ *   "id": "21bc3db4-8177-4004-b472-613d6239820d",
+ *   "email": "jdoe@example.com",
+ *   "createdAt": "2016-01-01T09:30:15Z",
+ *   "updatedAt": "2016-01-01T09:30:15Z"
+ * }
  */
 router.post('/', utils.notYetImplemented);
 
@@ -22,10 +46,34 @@ router.post('/', utils.notYetImplemented);
  * @api {GET} /users List users
  * @apiName ListUsers
  * @apiGroup Users
+ * @apiDescription Retrieve a paginated list of users.
  *
  * @apiUse Pagination
  * @apiUse Authorization
  * @apiUse UserResponse
+ *
+ * @apiSuccessExample Success 200
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ * Pagination-Offset: 0
+ * Pagination-Limit: 30
+ * Pagination-Total: 2
+ * Pagination-Filtered: 2
+ *
+ * [
+ *   {
+ *     "id": "21bc3db4-8177-4004-b472-613d6239820d",
+ *     "email": "jdoe@example.com",
+ *     "createdAt": "2016-01-01T09:30:15Z",
+ *     "updatedAt": "2016-01-01T09:30:15Z"
+ *   },
+ *   {
+ *     "id": "a774d438-13ed-4815-88bb-9a90f0558851",
+ *     "email": "jsmith@example.com",
+ *     "createdAt": "2016-01-01T09:32:15Z",
+ *     "updatedAt": "2016-01-01T09:34:15Z"
+ *   }
+ * ]
  */
 router.get('/', controller.index);
 
@@ -33,9 +81,21 @@ router.get('/', controller.index);
  * @api {GET} /users/:id Retrieve a user
  * @apiName RetrieveUser
  * @apiGroup Users
+ * @apiDescription Retrieve a specific user.
  *
  * @apiUse Authorization
  * @apiUse UserResponse
+ *
+ * @apiSuccessExample Success 200
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ *
+ * {
+ *   "id": "21bc3db4-8177-4004-b472-613d6239820d",
+ *   "email": "jdoe@example.com",
+ *   "createdAt": "2016-01-01T09:30:15Z",
+ *   "updatedAt": "2016-01-01T09:30:15Z"
+ * }
  */
 router.get('/:id', utils.notYetImplemented);
 
@@ -43,13 +103,42 @@ router.get('/:id', utils.notYetImplemented);
  * @api {PATCH} /users/:id Update a user
  * @apiName UpdateUser
  * @apiGroup Users
+ * @apiDescription Update some or all properties of a user.
+ *
+ * This resource is used for all user updates, including:
+ *
+ * * Changing the password.
+ * * Setting the initial password to complete the registration process.
+ *
+ * In that last case, the Bearer token supplied in the Authorization header should be
+ * the one found in the link sent to the user in the registration e-mail.
  *
  * @apiUse Authorization
  * @apiUse UserResponse
  *
- * @apiParam (JSON parameters) {String} [registrationOtp] The token sent to the user via e-mail on registration.
  * @apiParam (JSON parameters) {String{6..}} password The user's password.
  * @apiParam (JSON parameters) {String} [previousPassword] The user's previous password.
+ *
+ * @apiExample Example
+ * PATCH /api/users/21bc3db4-8177-4004-b472-613d6239820d HTTP/1.1
+ * Authorization: Bearer eyJhbGciOiJI.eyJzdWIiOiIx.Rq8IxqeX7eA6
+ * Content-Type: application/json
+ *
+ * {
+ *   "password": "letmein",
+ *   "previousPassword": "changeme"
+ * }
+ *
+ * @apiSuccessExample Success 201
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ *
+ * {
+ *   "id": "21bc3db4-8177-4004-b472-613d6239820d",
+ *   "email": "jdoe@example.com",
+ *   "createdAt": "2016-01-01T09:30:15Z",
+ *   "updatedAt": "2016-01-01T09:30:15Z"
+ * }
  */
 router.patch('/:id', utils.notYetImplemented);
 
