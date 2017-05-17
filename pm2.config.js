ed40d34e9a916986cadb21a8a1dd4cbe25850220
Simon Oulevay <simon.oulevay@gmail.com>
2017-05-17 09:54:36 +0200

    Fix pm2 cwd

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,9 @@ module.exports = {
   // http://pm2.keymetrics.io/docs/usage/application-declaration/
   apps: [
     {
-      name: 'BioSentiers Backend',
+      name: 'biosentiers-backend',
       script: 'bin/www',
+      cwd: '/var/www/biosentiers/backend/current',
       env: {
         NODE_ENV: 'production'
       }
