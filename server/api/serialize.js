cabb52e2ef5105128fb046469cef088072ed4b39
Simon Oulevay <simon.oulevay@gmail.com>
2017-05-19 17:40:20 +0200

    Check serialize req argument

--- server/api/serialize.js
+++ server/api/serialize.js
@@ -1,6 +1,12 @@
 const _ = require('lodash');
 
 module.exports = function(req, data, serializer) {
+  if (!req) {
+    throw new Error('The request object must be given as the first argument');
+  } else if (!req.app) {
+    throw new Error('The first argument does not appear to be an Express request object');
+  }
+
   if (_.isFunction(serializer.serialize)) {
     serializer = serializer.serialize;
   } else if (!_.isFunction(serializer)) {
