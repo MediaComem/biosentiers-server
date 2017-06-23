2f2375637779398709d79ab79c8764da0bf3a1ff
Simon Oulevay <simon.oulevay@gmail.com>
2017-06-23 10:33:28 +0200

    Migration to add description to themes

---
+++ migrations/20170623094014_add_name_to_themes.js
@@ -0,0 +1,28 @@
+const utils = require('../lib/knex-utils');
+
+exports.up = function(knex, Promise) {
+  utils.logMigration(knex);
+  return utils.sequentially(knex, () => {
+    return knex.schema.alterTable('theme', (t) => {
+      t.string('description', 50);
+    });
+  }, () => {
+    return Promise.all([
+      knex('theme').update('description', 'Oiseaux').where('name', 'bird'),
+      knex('theme').update('description', 'Papillons').where('name', 'butterfly'),
+      knex('theme').update('description', 'Fleures').where('name', 'flower'),
+      knex('theme').update('description', 'Arbres').where('name', 'tree')
+    ]);
+  }, () => {
+    return knex.schema.alterTable('theme', (t) => {
+      t.string('description', 50).notNullable().alter();
+    });
+  });
+};
+
+exports.down = function(knex, Promise) {
+  utils.logMigration(knex);
+  return knex.schema.alterTable('theme', (t) => {
+    t.dropColumn('description');
+  });
+};
