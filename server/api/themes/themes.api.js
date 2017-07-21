76ec9e583240678b8c132930bc2f15459e411d48
Simon Oulevay <simon.oulevay@gmail.com>
2017-07-21 17:46:11 +0200

    Filter themes by name

--- server/api/themes/themes.api.js
+++ server/api/themes/themes.api.js
@@ -4,6 +4,7 @@ const QueryBuilder = require('../query-builder');
 const route = require('../route');
 const serialize = require('../serialize');
 const Theme = require('../../models/theme');
+const utils = require('../utils');
 
 // API resource name (used in some API errors)
 exports.resourceName = 'theme';
@@ -11,6 +12,7 @@ exports.resourceName = 'theme';
 exports.list = route(async function(req, res) {
 
   const themes = await new QueryBuilder(req, res, policy.scope(req))
+    .filter(filterByNames)
     .paginate()
     .sorts('name')
     .defaultSort('name')
@@ -18,3 +20,13 @@ exports.list = route(async function(req, res) {
 
   res.send(await serialize(req, themes, policy));
 });
+
+function filterByNames(query, req) {
+
+  const names = utils.multiValueParam(req.query.name, _.isString);
+  if (!names.length) {
+    return;
+  }
+
+  return query.query(qb => qb.where('theme.name', 'in', names));
+}
