c15de4a83a03aad241ed341708ac1a2f7671bb09
Simon Oulevay <simon.oulevay@gmail.com>
2021-03-01 15:09:05 +0100

    Extract events by day and installation

--- scripts/extract-events.js
+++ scripts/extract-events.js
@@ -2,6 +2,7 @@
 const fs = require('fs-extra');
 const knex = require('knex');
 const _ = require('lodash');
+const moment = require('moment');
 const path = require('path');
 
 const config = require('../config');
@@ -38,7 +39,7 @@ async function createDatabase() {
 
 async function extractData(db) {
 
-  // Extract & save all events.
+  // Extract all events.
   const [ installations, events ] = await Promise.all([
     loadInstallations(db),
     loadInstallationEvents(db)
@@ -46,30 +47,77 @@ async function extractData(db) {
 
   await fs.mkdirs(dataDir);
 
+  const locationEvents = events.filter(event => event.type === 'location');
+
   await Promise.all([
     saveReadme(),
-    // Extract raw data.
+    // Save raw data.
     saveData('all-installations.json', installations),
     saveData('all-events.json', events),
-    // Extract non-location events.
+    // Save non-location events.
     saveData('other-events.json', events.filter(event => event.type !== 'location')),
-    // Extract location events as GeoJSON.
-    saveData('location-events.geojson', eventsToGeoJsonFeatureCollection(events.filter(event => event.type === 'location')))
+    // Save location events as GeoJSON.
+    saveData('location-events.geojson', eventsToGeoJsonFeatureCollection(locationEvents)),
+    // Save location event data by day as well.
+    saveEventDataGroupedByDayAndInstallation(locationEvents)
   ]);
 }
 
-function loadInstallations(db) {
-  return db('installation').select('id', 'api_id', 'properties', 'created_at', 'updated_at', 'first_started_at', 'last_event_at', 'events_count').orderBy('created_at', 'id');
+async function loadInstallations(db) {
+  const installations = await db('installation').select('api_id', 'properties', 'created_at', 'updated_at', 'first_started_at', 'last_event_at', 'events_count').orderBy('created_at', 'api_id');
+  return installations.map(installation => ({
+    ..._.omit(installation, 'id', 'api_id'),
+    id: installation.api_id
+  }));
 }
 
-function loadInstallationEvents(db) {
-  return db('installation_event').select().whereNot('type', 'test').orderBy('occurred_at', 'created_at', 'id');
+async function loadInstallationEvents(db) {
+  const events = await db('installation_event')
+    .innerJoin('installation', 'installation_event.installation_id', 'installation.id')
+    .select('installation_event.*', 'installation.api_id AS installation_api_id')
+    .whereNot('type', 'test')
+    .orderBy('occurred_at', 'created_at', 'api_id');
+
+  return events.map(event => ({
+    ..._.omit(event, 'id', 'api_id', 'installation_id', 'installation_api_id'),
+    id: event.api_id,
+    installation_id: event.installation_api_id
+  }));
 }
 
 async function saveData(basename, data, directory = dataDir) {
   const file = path.join(directory, basename);
   await fs.writeFile(file, JSON.stringify(data, undefined, 2), 'utf8');
   console.log(`Saved ${basename} to ${path.relative(config.root, directory)}`);
+}
+
+async function saveEventDataGroupedByDayAndInstallation(events) {
+  const eventsByDayAndInstallation = events.reduce(
+    (memo, event) => {
+      const occurredAt = moment(event.occurred_at, moment.ISO_8601);
+      if (!occurredAt.isValid()) {
+        throw new Error(`Event occurrence date ${JSON.stringify(event.occured_at)} is not a valid ISO-8601 date`);
+      }
+
+      const day = occurredAt.format('YYYY-MM-DD');
+      const installation = event.installation_id;
+      const key = `${day}---${installation}`;
+      memo[key] = memo[key] || [];
+      memo[key].push(event);
+      return memo;
+    },
+    {}
+  );
+
+  const byDayAndInstallationDir = path.join(dataDir, 'by-day-and-installation');
+  await fs.mkdirs(byDayAndInstallationDir);
+
+  await Promise.all(_.reduce(
+    eventsByDayAndInstallation,
+    (memo, events, key) =>
+      [ ...memo, saveData(`${key}.geojson`, eventsToGeoJsonFeatureCollection(events), byDayAndInstallationDir) ],
+    []
+  ));
 }
 
 function eventsToGeoJsonFeatureCollection(events) {
